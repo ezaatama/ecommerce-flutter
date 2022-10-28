730406b71127b181619abffffbba4e2cd5eac89d
Ezaatama <ezatama33@gmail.com>
2022-10-28 15:12:02 +0700

    screen on bottom nav

---
+++ lib/widgets/bottom_nav.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  BottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _bottomNavIndex = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
