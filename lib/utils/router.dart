226c97180f6bf3c92716152b58026a406ccde141
Ezaatama <ezatama33@gmail.com>
2022-11-04 11:46:14 +0700

    update route navigation

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,5 +1,11 @@
+import 'package:brand_marketplace_app/screens/bottom_screen.dart';
+import 'package:brand_marketplace_app/screens/favorite_screen.dart';
+import 'package:brand_marketplace_app/screens/homescreen.dart';
 import 'package:brand_marketplace_app/screens/login_screen.dart';
 import 'package:brand_marketplace_app/screens/onboarding_screen.dart';
+import 'package:brand_marketplace_app/screens/order_status_screen.dart';
+import 'package:brand_marketplace_app/screens/product_screen.dart';
+import 'package:brand_marketplace_app/screens/profile_screen.dart';
 import 'package:brand_marketplace_app/screens/register_screen.dart';
 import 'package:brand_marketplace_app/screens/splashscreen.dart';
 import 'package:brand_marketplace_app/screens/succes_regist_screen.dart';
@@ -18,6 +24,18 @@ class AppRoute {
         return MaterialPageRoute(builder: (_) => const RegisterScreen());
       case '/success-register':
         return MaterialPageRoute(builder: (_) => const SuccessRegistScreen());
+      case '/bottom-screen':
+        return MaterialPageRoute(builder: (_) => BottomScreen());
+      case '/homescreen':
+        return MaterialPageRoute(builder: (_) => const Homescreen());
+      case '/product':
+        return MaterialPageRoute(builder: (_) => const ProductScreen());
+      case '/order-status':
+        return MaterialPageRoute(builder: (_) => const OrderStatusScreen());
+      case '/favorite-screen':
+        return MaterialPageRoute(builder: (_) => const FavoriteScreen());
+      case '/profile-screen':
+        return MaterialPageRoute(builder: (_) => const ProfileScreen());
       default:
         return _errorRoute();
     }
